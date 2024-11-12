21b74a8cdbffcb834ca7c8256d9a5617cc6307d5
Walter Ruganzu <rwalter692@gmail.com>
2024-11-12 21:30:50 +0300

    Import the function-based view list_books. Import the class-based views BookListView and LibraryDetailView. Add URL patterns for these views.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from . import views
+from .views import list_books, BookListView, LibraryDetailView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('list_books/', views.list_books, name='list_books'),  # Function-based view
-    path('books/', views.BookListView.as_view(), name='book_list'),  # Function-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    path('list_books/', list_books, name='list_books'),  # Function-based view
+    path('books/', BookListView.as_view(), name='book_list'),  # Function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),  # Include relationship_app URLs
