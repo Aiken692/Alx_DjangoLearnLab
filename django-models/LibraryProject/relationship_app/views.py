6667fcb86060ad2912429752fcee26c8aa38f25a
Walter Ruganzu <rwalter692@gmail.com>
2024-11-12 21:21:23 +0300

    Corrected display for books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from django.views.generic import DetailView
+from django.views.generic.list import ListView
 from .models import Library
 
 # Create your views here.
@@ -8,9 +9,14 @@ def list_books(request):
     books = Book.objects.select_related('author').all()  # Fetch books with authors
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-def book_list_view(request):
+    def book_list_view(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/book_list.html', {'books': books})
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'relationship_app/book_list.html'
+    context_object_name = 'books'
 
 class LibraryDetailView(DetailView):
     model = Library
