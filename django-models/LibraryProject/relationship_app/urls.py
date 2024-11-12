bb108aed97e8d0cab284b3a8d90e1cb95db8a4d4
Walter Ruganzu <rwalter692@gmail.com>
2024-11-12 21:58:38 +0300

    This setup ensures that role-based access control is implemented within your Django application, managing different user roles and permissions effectively.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from .views import list_books, BookListView, LibraryDetailView
+from .views import list_books, BookListView, LibraryDetailView, admin_view, librarian_view, member_view
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Access control urls
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-    
+
     path('list_books/', list_books, name='list_books'),  # Function-based view
     path('books/', BookListView.as_view(), name='book_list'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
