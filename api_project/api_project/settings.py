ae9431264f7f7c7b8ea86bab289bd35f187da118
Walter Ruganzu <rwalter692@gmail.com>
2024-11-23 01:32:17 +0300

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,7 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # This ensures authentication is required by default
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
