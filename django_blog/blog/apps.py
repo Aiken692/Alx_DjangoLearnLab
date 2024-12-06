77062919f5b953d5d9b8db8389f201cfe82c8be0
Walter Ruganzu <rwalter692@gmail.com>
2024-12-06 20:23:29 +0300

    Renaming to blog

--- django_blog/blog/apps.py
+++ django_blog/blog/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BlogAppConfig(AppConfig):  # Ensure this class name matches your app name
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog_app'  # Ensure the name matches your app directory
+    name = 'blog'  # Ensure the name matches your app directory
