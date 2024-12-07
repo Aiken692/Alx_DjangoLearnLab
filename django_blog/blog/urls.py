4ef8e703698fa4aba69e0fe5968ed3c708363b84
Walter Ruganzu <rwalter692@gmail.com>
2024-12-07 13:26:15 +0300

    Refactoring urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,7 +3,6 @@ from django.contrib.auth import views as auth_views
 from . import views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
-
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
@@ -11,8 +10,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 
     path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
